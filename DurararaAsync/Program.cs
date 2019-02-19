f710dc02d80c2113f7ddc3acee505528b03e6f4d
N4teN <naten@gmx.de>
2019-02-19 11:07:21 +0100

    Fork Adoption
    - Changed Framework Target
    - Changed Botname + Appearance

--- DurararaAsync/Program.cs
+++ DurararaAsync/Program.cs
@@ -4,18 +4,17 @@ using DrrrAsync.Objects;
 
 namespace DrrrAsync
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            DrrrClient C = new DrrrClient("Welne Oren", "kuromu-2x");
+            DrrrClient C = new DrrrClient("NBot", "setton-2x");
             C.Login().GetAwaiter().GetResult();
 
             foreach (DrrrRoom Room in C.GetLounge().GetAwaiter().GetResult())
             {
                 if (Room.Name == "Abyss Bar 18+")
-                    C.JoinRoom(Room.RoomId).GetAwaiter().GetResult();
+                    /*C.JoinRoom(Room.RoomId).GetAwaiter().GetResult()/**/;
             }
 
             Console.ReadKey();
