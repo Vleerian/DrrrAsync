08c80c5b4d4a2f2c5d7e1b6b0f80af68cba88797
N4 <naten@gmx.de>
2019-02-20 12:24:55 +0100

    Make "PadBoth" an extension method for string

--- DurararaCore.cs/Utility.cs
+++ DurararaCore.cs/Utility.cs
@@ -33,21 +33,6 @@ namespace DrrrAsync
         }
 
         /// <summary>
-        /// Pads both sides of a string to center it.
-        /// </summary>
-        /// <param name="source">The source string</param>
-        /// <param name="length">How long you want the end string to be.</param>
-        /// <returns>The padded string.</returns>
-        public static string PadBoth(string source, int length)
-        {
-            if (source.Length > length)
-                throw new ArgumentException("Source string is longer than provided length.");
-            int spaces = length - source.Length;
-            int padLeft = spaces / 2 + source.Length;
-            return source.PadLeft(padLeft, ' ').PadRight(length, ' ');
-        }
-
-        /// <summary>
         /// Generates a colourful status box.
         /// </summary>
         /// <param name="code">The color code (see Utility.Col)</param>
@@ -55,7 +40,7 @@ namespace DrrrAsync
         public static void Status_box(char code, string text)
         {
             Col('w'); Console.Write("[");
-            Col(code); Console.Write(PadBoth(text, 7));
+            Col(code); Console.Write(text.PadBoth(7));
             Col('w'); Console.Write("] ");
         }
 
@@ -86,6 +71,24 @@ namespace DrrrAsync
                 Status_box(status_colour, status);
                 Console.Write(text + "\n");
             }
+        }
+    }
+
+    public static class UtilityExtensions
+    {
+        /// <summary>
+        /// Pads both sides of a string to center it.
+        /// </summary>
+        /// <param name="source">The source string</param>
+        /// <param name="length">How long you want the end string to be.</param>
+        /// <returns>The padded string.</returns>
+        public static string PadBoth(this string source, int length)
+        {
+            if (source.Length > length)
+                throw new ArgumentException("Source string is longer than provided length.");
+            int spaces = length - source.Length;
+            int padLeft = spaces / 2 + source.Length;
+            return source.PadLeft(padLeft, ' ').PadRight(length, ' ');
         }
     }
 }
