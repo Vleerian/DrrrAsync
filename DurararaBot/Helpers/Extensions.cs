748b6545f843f6ae8f0b618989bbd88bec50b698
N4 <naten@gmx.de>
2019-02-23 00:04:42 +0100

    Fix, Adjustment
    - Fix Has-Attribute-Extensions
    - Document CreateDelegate and give object a default-value (For static delegates)

--- DurararaBot/Helpers/Extensions.cs
+++ DurararaBot/Helpers/Extensions.cs
@@ -7,7 +7,13 @@ namespace DrrrAsync.Bot
 {
     public static class Extensions
     {
-        public static Delegate CreateDelegate(this MethodInfo methodInfo, object target)
+        /// <summary>
+        /// Creates a dynamically typed delegate from just the 
+        /// method info provided on the specified target.
+        /// </summary>
+        /// <param name="methodInfo">The method to create a delegate for</param>
+        /// <param name="target">The target to create the delegate for, if non-static.</param>
+        public static Delegate CreateDelegate(this MethodInfo methodInfo, object target = null)
         {
             var getType = methodInfo.ReturnType == typeof(void)
                 ? (Func<Type[], Type>) Expression.GetActionType
@@ -22,21 +28,22 @@ namespace DrrrAsync.Bot
             else return Delegate.CreateDelegate(getType(types.ToArray()), target, methodInfo.Name);
         }
 
-        private static bool Has<T>(dynamic attributeHolder, out T foundAttribute) where T : Attribute
+        public static bool Has<T>(this MemberInfo member) where T : Attribute => member.Has<T>(out _);
+        public static bool Has<T>(this MemberInfo member, out T foundAttribute) where T : Attribute
         {
-            foundAttribute = attributeHolder.GetCustomAttribute<T>();
+            foundAttribute = member.GetCustomAttribute<T>();
             if (foundAttribute != null)
                 return true;
             return false;
         }
 
-        public static bool Has<T>(this MemberInfo member, out T foundAttribute) where T : Attribute =>
-            Has(member, out foundAttribute);
-        public static bool Has<T>(this MemberInfo member) where T : Attribute =>
-            member.Has<T>(out _);
-        public static bool Has<T>(this Type type, out T foundAttribute) where T : Attribute =>
-            Has(type, out foundAttribute);
-        public static bool Has<T>(this Type type) where T : Attribute =>
-            type.Has<T>(out _);
+        public static bool Has<T>(this Type type) where T : Attribute => type.Has<T>(out _);
+        public static bool Has<T>(this Type type, out T foundAttribute) where T : Attribute
+        {
+            foundAttribute = type.GetCustomAttribute<T>();
+            if (foundAttribute != null)
+                return true;
+            return false;
+        }
     }
 }
